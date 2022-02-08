cf3f7b71cf7d6dd9f5c731955edd6c061f6834dc
SireenAtGithub <er.sireen@gmail.com>
2022-02-09 00:19:28 +0530

    Redefined PHP files

--- date_filter.php
+++ date_filter.php
@@ -1,34 +1,18 @@
 <?php
     include "config.php";
+    include "functions.php";
     $start_date = $_REQUEST['start_date'];
     $end_date = $_REQUEST['end_date'];
     $page = $_REQUEST['page'];
-    $limit = 5;
+
+    $totalPageQuery = "SELECT COUNT(*) FROM user_detail WHERE dob BETWEEN '$start_date' AND '$end_date'";
+    $total_pages = get_total_pages($totalPageQuery);
+     
+    $query_str = "SELECT * FROM user_detail WHERE dob BETWEEN '$start_date' AND '$end_date'";
+    $return_arr = get_return_data($query_str, $page);
     
-    $initial_page = ($page-1) * $limit; 
-    $query = "SELECT COUNT(*) FROM user_detail WHERE dob BETWEEN '$start_date' AND '$end_date'";
-    $result = mysqli_query($con, $query);
-    $row = mysqli_fetch_row($result);
-    $total_rows = $row[0];
-    $total_pages = ceil($total_rows / $limit);
-    $query_str = "SELECT * FROM user_detail WHERE dob BETWEEN '$start_date' AND '$end_date' LIMIT $initial_page, $limit ";
-    
-    $data_array = array();
-    $return_arr = array();
-    
-    $result = mysqli_query($con,$query_str);
-    while($row = mysqli_fetch_array($result)){
-        $id = $row['id'];
-        $user_name = $row['user_name'];
-        $gender = $row['gender'];
-        $dob = $row['dob'];
-        $data_array[] = array("id" => $id,
-                        "user_name" => $user_name,
-                        "gender" => $gender,
-                        "dob" => $dob);
-    }
-    $return_arr[] = array("data" => $data_array);
     $return_arr[] = array("total_pages" => $total_pages);
     echo json_encode($return_arr);
     $con -> close();
+    
 ?>
---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+    include "config.php";
+    $limit = 5;
+    function    get_total_pages($str_query){
+        global  $con;
+        global  $limit;
+        $query = $str_query;
+        $result = mysqli_query($con, $query);
+        $row = mysqli_fetch_row($result);
+        $total_rows = $row[0];
+        $total_pages = ceil($total_rows / $limit);
+        return  $total_pages;
+    }
+
+    function get_return_data($str_query, $page){
+        global  $limit;
+        $initial_page = ($page-1) * $limit;
+        $data_array = array();
+        $return_arr = array();
+        global  $con;
+        $final_query = $str_query . " LIMIT $initial_page, $limit";
+        $result = mysqli_query($con,$final_query);
+        while($row = mysqli_fetch_array($result)){
+            $id = $row['id'];
+            $user_name = $row['user_name'];
+            $gender = $row['gender'];
+            $dob = $row['dob'];
+            $data_array[] = array("id" => $id,
+                            "user_name" => $user_name,
+                            "gender" => $gender,
+                            "dob" => $dob);
+        }
+        $return_arr[] = array("data" => $data_array);
+        return  $return_arr;
+    }
+?>
